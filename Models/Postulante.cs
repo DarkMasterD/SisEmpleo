3b17964ca0cdcbf13840b707e2713f96aa9038ae
Paola Vidal <paola.vidal@catolica.edu.sv>
2025-04-08 21:14:02 -0600

    Historia 6 finalizada

--- Models/Postulante.cs
+++ Models/Postulante.cs
@@ -9,5 +9,7 @@ namespace SisEmpleo.Models
         public int id_usuario { get; set; }
         public string nombre { get; set; }
         public string direccion {  get; set; }
+
+        public string NivelExperiencia { get; set; }
     }
 }
---
+++ Models/PostulanteViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SisEmpleo.Models
+{
+    public class PostulanteViewModel
+    {
+        public int IdPostulante { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public int IdUsuario { get; set; }
+        public string NivelExperiencia { get; set; }
+    }
+}
